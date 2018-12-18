68e674cc1041a45bb2cf86bdf9085770f61c74a3
Rex Jomy Joseph <rexjj7@gmail.com>
2018-12-18 11:03:18 -0800

    Add files via upload
    Digit identification is done in this file.

---
+++ digitidentifier.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+from scipy.interpolate import griddata
+import time
+
+
+def predictor(im,model):
+    
+    out = np.zeros(im.shape,np.uint8)
+    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
+    image, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+      if cv2.contourArea(cnt)>50:
+        [x,y,w,h] = cv2.boundingRect(cnt)
+        if  h>28:
+            cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+            roi = thresh[y:y+h,x:x+w]
+            roismall = cv2.resize(roi,(10,10))
+            roismall = roismall.reshape((1,100))
+            roismall = np.float32(roismall)
+            retval, results, neigh_resp, dists = model.findNearest(roismall, k = 1)
+            string = str(int((results[0][0])))
+            if string=="":
+                return 0          
+            return int(results[0][0])
+    return 0
+
+
+def train():
+    samples = np.loadtxt('generalsamples.data',np.float32)
+    responses = np.loadtxt('generalresponses.data',np.float32)
+    responses = responses.reshape((responses.size,1))
+    model = cv2.ml.KNearest_create()
+    model.train(samples,cv2.ml.ROW_SAMPLE,responses)
+    return model
+
+
