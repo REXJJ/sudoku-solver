1da7e340068da8001eb0d0931bf9e01e6974d050
Rex Jomy Joseph <rexjj7@gmail.com>
2018-12-20 23:25:18 -0800

    Update digitidentifier.py

--- digitidentifier.py
+++ digitidentifier.py
@@ -3,7 +3,6 @@ import numpy as np
 from scipy.interpolate import griddata
 import time
 
-
 def predictor(im,model):
     
     out = np.zeros(im.shape,np.uint8)
@@ -35,4 +34,31 @@ def train():
     model.train(samples,cv2.ml.ROW_SAMPLE,responses)
     return model
 
+def update(sudoku):
+    samples = np.loadtxt('generalsamples.data',np.float32)
+    responses = np.loadtxt('generalresponses.data',np.float32)
+    for i in range(9):
+        for j in range(9):
+            file='./CROP/square'+str(i)+str(j)+'.png'
+            im=cv2.imread(file)
+            out = np.zeros(im.shape,np.uint8)
+            gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+            thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
+            image, contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in contours:
+              if cv2.contourArea(cnt)>50:
+                [x,y,w,h] = cv2.boundingRect(cnt)
+                if  h>28:
+                    cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+                    roi = thresh[y:y+h,x:x+w]
+                    roismall = cv2.resize(roi,(10,10))
+                    roismall = roismall.reshape((1,100))
+                    #roismall = np.float32(roismall)
+                    if sudoku[i][j]!=0:
+                        samples=np.append(samples,roismall,0)
+                        responses=np.append(responses,np.array(float(sudoku[i][j])))
 
+    
+
+    np.savetxt('generalsamples.data',samples)
+    np.savetxt('generalresponses.data',responses)    
